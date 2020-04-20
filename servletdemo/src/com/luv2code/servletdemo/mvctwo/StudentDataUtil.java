0a337245450878a2bbd684c7bbe55d77bbba5e4f
Pritam Sinha <Pritam Sinha@DESKTOP-4M9TNSB>
2020-04-21 00:54:56 +0530

    servletdemo

---
+++ servletdemo/src/com/luv2code/servletdemo/mvctwo/StudentDataUtil.java
@@ -0,0 +1,18 @@
+package com.luv2code.servletdemo.mvctwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataUtil {
+
+	public static List<Student> getStudents(){
+		
+		List<Student> students = new ArrayList<Student>();
+		
+		students.add(new Student("Mary","Public","mary@luv2code.com"));
+		students.add(new Student("John","Doe","john@luv2code.com"));
+		students.add(new Student("Ajay","Rao","ajay@luv2code.com"));
+		
+		return students;
+	}
+}
